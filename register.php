35859eca122a021b80714c1314095fdc259fb5a3
Dominik Patryas <dominik.patryas@gmail.com>
2019-04-13 18:15:54 +0200

    added jquery slide and updated login/register layout

--- register.php
+++ register.php
@@ -12,88 +12,122 @@ require 'includes/form_handlers/login_handler.php';
 
 <head>
 
-<link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
-
+    <link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="assets/js/register.js"></script>
 </head>
 
 
 
 <body>
+            // keep register form id="first" show
+    <?php
+        if (isset($_POST['register_button'])) {
+            echo '
+            
+            <script> 
+            
+            $(document).ready(function() {
+                $("#first").hide();
+                $("#second").show();
+
+            });
+            
+            </script>
+
+            ';
+        }
+
+    ?>
+
+
     <div class="wrapper">
         <div class="login_box">
-            <form action="register.php" method="POST">
+            <div class="login_header">
+                <h2>Swirlfeeed!</h2>
+                Login or sign up
+            </div>
 
-                <input type="email" name="log_email" placeholder="Email Address" value="<?php
-                if(isset($_SESSION['log_email'])) {
+            <div id="first">
+                <form action="register.php" method="POST">
+
+                    <input type="email" name="log_email" placeholder="Email Address" value="<?php
+                    if(isset($_SESSION['log_email'])) {
                     echo $_SESSION['log_email'];
-                } ?>" required> <br>
+                      } ?>" required> <br>
 
 
-                <input type="password" name="log_password" placeholder="Password" > <br>
-                <input type="submit" name="login_button" value="Login">
+                    <input type="password" name="log_password" placeholder="Password"> <br>
+                    <input type="submit" name="login_button" value="Login"> <br>
+                    <a href="#" id="signup" class="signup">Sign up here!</a>
 
-                <?php  if (in_array("Email or username is incorrect<br>", $error_array)) {
+                    <?php  if (in_array("Email or username is incorrect<br>", $error_array)) {
                     echo "Email or username is incorrect<br>";
-                } ?>
+                     } ?>
 
-            </form>
-
-
-
-            <form action="register.php" method="POST">
-
-                <input type="text" name="reg_fname" placeholder="First Name" value="<?php
-                if(isset($_SESSION['reg_fname'])) {
-                    echo $_SESSION['reg_fname'];
-                } ?>" required>
-                <br>
-
-                <?php if (in_array("Too long or too short first name", $error_array)) echo "Too long or too short first name<br>"; ?>
-
-                <input type="text" name="reg_lname" placeholder="Last Name" value="<?php
-                if(isset($_SESSION['reg_lname'])) {
-                    echo $_SESSION['reg_lname'];
-                } ?>" required>
-                <br>
-
-                <?php if (in_array("Too long or too short last name<br>", $error_array)) echo "Too long or too short last name<br>"; ?>
+                </form>
+            </div>
 
 
-                <input type="email" name="reg_email" placeholder="Email" value="<?php
-                if(isset($_SESSION['reg_email'])) {
-                    echo $_SESSION['reg_email'];
-                } ?>" required>
-                <br>
+            <div id="second">
+                <form action="register.php" method="POST">
 
-                <input type="email" name="reg_email2" placeholder="Confirm email" value="<?php
-                if(isset($_SESSION['reg_email2'])) {
-                    echo $_SESSION['reg_email2'];
-                } ?>" required>
-                <br>
+                    <input type="text" name="reg_fname" placeholder="First Name" value="<?php
+                    if(isset($_SESSION['reg_fname'])) {
+                        echo $_SESSION['reg_fname'];
+                    } ?>" required>
+                    <br>
 
-                <?php if (in_array("Email already in use<br>", $error_array)) echo "Email already in use<br>"; 
-                else if (in_array("Invalid email format<br>", $error_array)) echo "Invalid email format<br>"; 
-                else if (in_array("Emails don't match!<br>", $error_array)) echo "Emails don't match!<br>"; ?>
+                    <?php if (in_array("Too long or too short first name", $error_array)) echo "Too long or too short first name<br>"; ?>
 
+                    <input type="text" name="reg_lname" placeholder="Last Name" value="<?php
+                    if(isset($_SESSION['reg_lname'])) {
+                        echo $_SESSION['reg_lname'];
+                    } ?>" required>
+                    <br>
 
-                <input type="password" name="reg_password" placeholder="Password" required>
-                <br>
-                <input type="password" name="reg_password2" placeholder="Confirm Password" required>
-                <br>
-
-                <?php if (in_array("Passwords do not match<br>", $error_array)) echo "Passwords do not match<br>"; 
-                else if (in_array("Your password can only contain english characters or numbers<br>", $error_array)) echo "Your password can only contain english characters or numbers<br>"; 
-                else if (in_array("Your password must be between 5 and 30 characters<br>", $error_array)) echo "Your password must be between 5 and 30 characters<br>"; ?>
-
-                <input type="submit" name="register_button" value="Register">
-
-                <?php if (in_array("<span style='color: #14C800;'> You registered correctly. </span><br>", $error_array)) echo "<span style='color: #14C800;'> You registered correctly. </span><br>"; ?>
-
-                
+                    <?php if (in_array("Too long or too short last name<br>", $error_array)) echo "Too long or too short last name<br>"; ?>
 
 
+                    <input type="email" name="reg_email" placeholder="Email" value="<?php
+                    if(isset($_SESSION['reg_email'])) {
+                        echo $_SESSION['reg_email'];
+                    } ?>" required>
+                    <br>
 
-            </form>
+                    <input type="email" name="reg_email2" placeholder="Confirm email" value="<?php
+                    if(isset($_SESSION['reg_email2'])) {
+                        echo $_SESSION['reg_email2'];
+                    } ?>" required>
+                    <br>
+
+                    <?php if (in_array("Email already in use<br>", $error_array)) echo "Email already in use<br>"; 
+                    else if (in_array("Invalid email format<br>", $error_array)) echo "Invalid email format<br>"; 
+                    else if (in_array("Emails don't match!<br>", $error_array)) echo "Emails don't match!<br>"; ?>
+
+
+                    <input type="password" name="reg_password" placeholder="Password" required>
+                    <br>
+                    <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+                    <br>
+
+                    <?php if (in_array("Passwords do not match<br>", $error_array)) echo "Passwords do not match<br>"; 
+                    else if (in_array("Your password can only contain english characters or numbers<br>", $error_array)) echo "Your password can only contain english characters or numbers<br>"; 
+                    else if (in_array("Your password must be between 5 and 30 characters<br>", $error_array)) echo "Your password must be between 5 and 30 characters<br>"; ?>
+
+                    <input type="submit" name="register_button" value="Register">
+
+                    <?php if (in_array("<span style='color: #14C800;'> You registered correctly. </span><br>", $error_array)) echo "<span style='color: #14C800;'> You registered correctly. </span><br>"; ?>
+                    <a href="#" id="signin" class="signin">Already have an account? Sign in here!</a>
+
+
+
+
+
+                </form>
+            </div>
+
+
         </div>
     </div>
 
