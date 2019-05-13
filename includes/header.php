a662387c24a0b88f4679ca708c96856cefc10588
Dominik Patryas <dominik.patryas@gmail.com>
2019-05-13 21:13:26 +0200

    upload profile photo

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,8 @@ else {
 <script src="assets/js/bootstrap.js"> </script>
 <script src="assets/js/bootbox.min.js"> </script>
 <script src="assets/js/demo.js"> </script>
+<script src="assets/js/jcrop_bits.js"> </script>
+<script src="assets/js/jquery.Jcrop.js"> </script>
 
 <!-- CSS -->
 
