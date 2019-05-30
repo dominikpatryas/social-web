d59e4251da9d51c3e607ddeac18ebb155db948e6
Dominik Patryas <dominik.patryas@gmail.com>
2019-05-30 13:50:19 +0200

    insert post notification

--- includes/classes/Notification.php
+++ includes/classes/Notification.php
@@ -16,6 +16,35 @@
         $query = mysqli_query($this->con, "SELECT * FROM Notifications WHERE user_to = '$userLoggedIn' and viewed='no'");
         return mysqli_num_rows($query);
     }
+
+    public function insertNotification($post_id, $user_to, $type) {
+        $userLoggedIn = $this->user_obj->getUsername();
+        $userLoggedInName = $this->user_obj->getFirstAndLastName();
+
+        $date_time = new Date("Y-m-d H:i:s");
+
+        switch($type) {
+            case 'comment':
+                $message = $userLoggedIn . "commented your post";
+                break;
+            case 'like':
+                $message = $userLoggedIn . "liked your post";
+                break;
+            case 'profile':
+                $message = $userLoggedIn . "posted on your profile";
+                break;
+            case 'comment_non_owner':
+                $message = $userLoggedIn . "commented on a post you commented on";
+                break;
+            case 'profile_comment':
+                $message = $userLoggedIn . "commented on your profile's post";
+                break;
+        }
+
+        $link = "post.php?=" . $post_id;
+        $insert_query = mysqli_query($this->con, "INSERT INTO notifications VALUES ('', '$user_to', '$userLoggedIn', '$message', '$link'
+        , '$date_time', 'no', 'no')");
+    }
 }
 
 ?>
